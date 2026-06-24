9bbde5d8fb7d1ee32ff3df33e319d154d6d0cc26
vlgoloVH <2hv6thvvwn@privaterelay.appleid.com>
2026-06-24 21:18:25 +0300

    Update reveal.tsx

--- components/ui/reveal.tsx
+++ components/ui/reveal.tsx
@@ -25,26 +25,3 @@ export function Reveal({
     </motion.div>
   );
 }
-
-export function RevealText({
-  children,
-  delay = 0,
-  className,
-  animate = true,
-}: {
-  children: ReactNode;
-  delay?: number;
-  className?: string;
-  animate?: boolean;
-}) {
-  return (
-    <motion.div
-      className={className}
-      initial={{ opacity: 0, x: -40 }}
-      animate={{ opacity: animate ? 1 : 0, x: animate ? 0 : -40 }}
-      transition={{ duration: 0.8, delay, ease: [0.25, 1, 0.5, 1] }}
-    >
-      {children}
-    </motion.div>
-  );
-}
