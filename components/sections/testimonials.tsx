dd655d064729392166ca1890c27f072c39979ed5
vlgoloVH <2hv6thvvwn@privaterelay.appleid.com>
2026-07-15 21:48:44 +0200

    fix: align testimonials trailing spacer with content width like case study galleries

--- components/sections/testimonials.tsx
+++ components/sections/testimonials.tsx
@@ -134,7 +134,7 @@ export function Testimonials() {
             {doubled.map((t, i) => (
               <TestimonialCard key={`${t.name}-${i}`} quote={t.quote} name={t.name} role={t.role} />
             ))}
-           <div style={{ width: "max(3rem, calc(50vw - 700px + 1.5rem))", flexShrink: 0 }} />
+           <div style={{ width: "max(1.5rem, calc(50vw - 700px))", flexShrink: 0 }} />
           </div>
         </div>
         <div
